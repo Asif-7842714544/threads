1d5e14a3aeabad9ce27d74f588f819c1547ddca1
Your Name <you@example.com>
2023-08-21 15:05:33 +0530

    y

--- app/(root)/create-thread/page.tsx
+++ app/(root)/create-thread/page.tsx
@@ -3,7 +3,7 @@ import PostThread from "@/components/shared/PostThread";
 async function page() {
   return (
     <div>
-      <h4 className="text-red-500 ">Create Thread</h4>
+      <h4 className="text-red-500 text-heading2-bold ">Create Thread</h4>
       <PostThread />
     </div>
   );
